42f300825826cdae602d6bb2014e259296194254
cj2030010002-xgk <73773332+cj2030010002-xgk@users.noreply.github.com>
2020-11-02 18:56:00 +0800

    Add files via upload

--- 4,5测试.R
+++ 4,5测试.R
@@ -9,36 +9,38 @@ print(x,c(1,5,9,15))
 a <- c(10:0,1:10)
 diag(a)
 
-###################
-#5.1
-x <- 0:99
-sqrt_x <- sqrt(x)
-is_square_number <- sqrt_x == floor(sqrt_x)
-square_numbers <- x[is_square_number]
-groups <- cut(
-  square_numbers,
-  seq.int(min(x),max(x),10),
-  include.lowest = TRUE,
-  right = FALSE)
-split(square_numbers,groups)
-#5.2
-new_data_frame <- iris
-new_data_frame
-mean(c(new_data_frame[1:150,1]))
-mean(c(new_data_frame[1:150,2]))
-mean(c(new_data_frame[1:150,3]))
-mean(c(new_data_frame[1:150,4]))
-#5.3
-x <- beaver1
-X <- data.frame(id=rep(1,each=114))
-new_beaver1 <- cbind(X,x)
-new_beaver1
-y <- beaver2
-Y <- data.frame(id=rep(2,each=100))
-new_beaver2 <- cbind(y,Y)
-new_beaver2
-new_beaver3 <- rbind(new_beaver1,new_beaver2)
-data.frame(new_beaver3,activ=1)
+###############################
+#5.1test
+(a_list <- list(
+  first = (0:9)^2,
+  second = (10:19)^2,
+  third = (20:29)^2,
+  four = (30:39)^2,
+  five = (40:49)^2,
+  six = (50:59)^2,
+  seven = (60:69)^2,
+  eight = (70:79)^2,
+  nine = (80:89)^2,
+  ten = (90:99)^2
+))
 
+#5.2test
+iris_data_frame <- data.frame(
+  x = iris
+)
+colSums(iris_data_frame[,1:2])
 
+#5.3test
+m_data_frame <- data.frame(
+  x = beaver1,
+  ID = 1
+)
+
+n_data_frame <- data.frame(
+  x = beaver2,
+  ID = 2
+)
+rbind(m_data_frame,n_data_frame)
+h_data_frame <- rbind(m_data_frame,n_data_frame)
+subset(h_data_frame,x.activ>0)
 
